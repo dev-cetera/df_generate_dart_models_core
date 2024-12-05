1c32b83aa48e536e677e3313a30198ef08b0ec6b
robmllze <86869208+robmllze@users.noreply.github.com>
2024-12-05 12:32:49 +1100

    add test

---
+++ test/test_decompose_dart_collection_type.dart
@@ -0,0 +1,12 @@
+
+import 'package:df_generate_dart_models_core/df_generate_dart_models_core_utils.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('decomposeDartCollectionType', () {
+    test('1', () {
+      final result = decomposeDartCollectionType('List<String>');
+      expect(result.toString(), '([List<String>, List, String])');
+    });
+  });
+}
